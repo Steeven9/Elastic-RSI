f52649608160682f9919d916dd2bb3c0b6295c74
Stefano Taillefert <stefano.taille@gmail.com>
2022-05-07 12:15:47 +0200

    Improve script

--- data/convert_all.sh
+++ data/convert_all.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-SPLIT_LINES=500
+# How to use:
+#     ./convert_all.sh <files>
 
+SPLIT_LINES=10000
+
+echo "This might take a while, depending on file size"
 for f in $@; do \
+    echo "Splitting and converting ${f}..."
     split -l $SPLIT_LINES $f "${f}.part."
-    for p in $(find . -maxdepth 1 -type f -name "${f}\.part\.\w\w"); do \
+    for p in $(find . -maxdepth 1 -type f -name "${f}\.part\.*"); do \
         ./conversion.sh $p > "$p.ndjson"
     done
 done
+
+echo "Done!"
