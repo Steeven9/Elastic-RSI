e3408c6d631de3cf9bacb70e319f2ab8afcfdbd7
Stefano Taillefert <stefano.taille@gmail.com>
2022-05-16 11:55:21 +0200

    Add aggregations endpoint

--- server/routes/elastic.js
+++ server/routes/elastic.js
@@ -28,7 +28,11 @@ const validateAuth = (auth) => {
     return false;
   }
   const [type, token] = auth.split(" ");
-  return !(type !== "Bearer" || token.length === 0 || token !== process.env.REACT_APP_BACKEND_API_TOKEN);
+  return !(
+    type !== "Bearer" ||
+    token.length === 0 ||
+    token !== process.env.REACT_APP_BACKEND_API_TOKEN
+  );
 };
 
 // Returns the status of the cluster
@@ -37,27 +41,20 @@ router.get("/status", async (_req, res) => {
   res.send(helth);
 });
 
-// Returns the search results for the given query
-router.get("/search", async (req, res) => {
+// Generic endpoint that accepts a query object
+router.get("/aggs", async (req, res) => {
   // Validate "auth"
   if (!validateAuth(req.headers.authorization)) {
     res.status(401).send("Missing or malformed auth token");
     return;
   }
 
-  // Validate request
-  if (typeof req.body.queryString !== "string") {
-    res.status(400).send('Missing parameter "body.queryString"');
-    return;
-  }
-
   try {
     const response = await client.search(
       {
+        size: 0,
         index: INDEX,
-        query: {
-          match: { field: req.body.queryString },
-        },
+        aggs: req.body,
       },
       {
         headers: {
@@ -65,7 +62,7 @@ router.get("/search", async (req, res) => {
         },
       }
     );
-    res.status(200).json(response.hits.hits);
+    res.status(200).json(response.aggregations);
   } catch (err) {
     res.status(500).send(err.meta.body);
   }
@@ -80,15 +77,12 @@ router.get("/get", async (req, res) => {
   }
 
   try {
-    const response = await client.search(
-      req.body,
-      {
-        headers: {
-          Authorization: `ApiKey ${process.env.ELASTIC_API_KEY}`,
-        },
-      }
-    );
-    res.status(200).json(response.hits.hits);
+    const response = await client.search(req.body, {
+      headers: {
+        Authorization: `ApiKey ${process.env.ELASTIC_API_KEY}`,
+      },
+    });
+    res.status(200).json(response);
   } catch (err) {
     res.status(500).send(err.meta.body);
   }
