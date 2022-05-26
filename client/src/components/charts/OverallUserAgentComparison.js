fa024daafa2a8309bdf7183323a3fbdf7ad7e5d7
bohnhofl <bohnhofl@student.ethz.ch>
2022-05-26 19:22:09 +0200

    add new charts, add query and fix eslint warnings

--- client/src/components/charts/OverallUserAgentComparison.js
+++ client/src/components/charts/OverallUserAgentComparison.js
@@ -94,7 +94,8 @@ function OverallUserAgentComparison() {
                 type: "bar",
                 tooltip: {
                   formatter: (params) =>
-                    `Device type: ${params.name} </br>Number requests: ${params.value
+                    `Device type: ${params.name} </br>Number requests: ${
+                      params.value
                     } </br> Overall share: ${(
                       (100 * params.value) /
                       data.counts.reduce((a, e) => a + e, 0)
--- client/src/components/charts/WeekdayCounts.js
+++ client/src/components/charts/WeekdayCounts.js
@@ -12,108 +12,108 @@ import buildQuery from "../../utils/query";
 import { useSelector } from "react-redux";
 
 function dateToWeekDayIndex(date) {
-    const index = date.getDay();
-    return (index + 6) % 7;
+  const index = date.getDay();
+  return (index + 6) % 7;
 }
 
 function WeekDayCounts() {
-    const [rows, setRows] = useState(undefined);
-    const countryFilter = useSelector((st) => st.generalReducer.countryFilter);
-    const regionFilter = useSelector((st) => st.generalReducer.regionFilter);
+  const [rows, setRows] = useState(undefined);
+  const countryFilter = useSelector((st) => st.generalReducer.countryFilter);
+  const regionFilter = useSelector((st) => st.generalReducer.regionFilter);
 
-    useEffect(() => {
-        async function fetchData() {
-            const query = buildQuery(
-                {
-                    country: countryFilter,
-                    admin1: regionFilter,
-                },
-                {
-                    aggs: {
-                        by_day: {
-                            date_histogram: {
-                                field: "ch_date",
-                                calendar_interval: "day",
-                            },
-                        },
-                    },
-                    sort: [
-                        {
-                            ch_date: {
-                                order: "asc",
-                            },
-                        },
-                    ],
-                }
-            );
-            const response = await getWithQuery(query);
-            let counts = new Array(7).fill(0);
-            const numberDays = response.aggregations.by_day.buckets.length;
-            const startDate = new Date(
-                response.aggregations.by_day.buckets[0].key_as_string
-            );
-            let weekDayIndex = dateToWeekDayIndex(startDate);
-            for (let i = 0; i < numberDays; i++) {
-                counts[weekDayIndex] += 1;
-                weekDayIndex = (weekDayIndex + 1) % 7;
-            }
-            const result = [
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday",
-                "Sunday",
-            ].map((dayName, dayIndex) => {
-                return {
-                    name: dayName,
-                    count: counts[dayIndex],
-                };
-            });
-            setRows(result);
+  useEffect(() => {
+    async function fetchData() {
+      const query = buildQuery(
+        {
+          country: countryFilter,
+          admin1: regionFilter,
+        },
+        {
+          aggs: {
+            by_day: {
+              date_histogram: {
+                field: "ch_date",
+                calendar_interval: "day",
+              },
+            },
+          },
+          sort: [
+            {
+              ch_date: {
+                order: "asc",
+              },
+            },
+          ],
         }
-        fetchData();
-    }, [countryFilter, regionFilter]);
+      );
+      const response = await getWithQuery(query);
+      let counts = new Array(7).fill(0);
+      const numberDays = response.aggregations.by_day.buckets.length;
+      const startDate = new Date(
+        response.aggregations.by_day.buckets[0].key_as_string
+      );
+      let weekDayIndex = dateToWeekDayIndex(startDate);
+      for (let i = 0; i < numberDays; i++) {
+        counts[weekDayIndex] += 1;
+        weekDayIndex = (weekDayIndex + 1) % 7;
+      }
+      const result = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ].map((dayName, dayIndex) => {
+        return {
+          name: dayName,
+          count: counts[dayIndex],
+        };
+      });
+      setRows(result);
+    }
+    fetchData();
+  }, [countryFilter, regionFilter]);
 
-    return (
-        <div
-            style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-            }}
-        >
-            {rows ? (
-                <TableContainer sx={{ width: "20%" }} component={Paper}>
-                    <Table aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>
-                                    <b>Week day</b>
-                                </TableCell>
-                                <TableCell align="left">
-                                    <b>Number occurences</b>
-                                </TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows.map((row) => (
-                                <TableRow key={row.name}>
-                                    <TableCell component="th" scope="row">
-                                        {row.name}
-                                    </TableCell>
-                                    <TableCell align="center">{row.count}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            ) : (
-                <div style={{ textAlign: "center" }}>Loading...</div>
-            )}
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {rows ? (
+        <TableContainer sx={{ width: "20%" }} component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <b>Week day</b>
+                </TableCell>
+                <TableCell align="left">
+                  <b>Number occurences</b>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="center">{row.count}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <div style={{ textAlign: "center" }}>Loading...</div>
+      )}
+    </div>
+  );
 }
 
 export default WeekDayCounts;
