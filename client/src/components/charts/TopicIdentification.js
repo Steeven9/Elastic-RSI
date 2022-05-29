a23a6bf8adcbb150df34cbde2041a70c4c3baf00
bohnhofl <bohnhofl@student.ethz.ch>
2022-05-29 19:19:51 +0200

    add topic identification precision as MUI card

--- client/src/components/charts/TopicIdentification.js
+++ client/src/components/charts/TopicIdentification.js
@@ -1,52 +1,75 @@
 import React, { useEffect, useState } from "react";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 import { getCount } from "../../API";
 import Loading from "../Loading";
 
 function TopicIdentification() {
-  const [data, setData] = useState(undefined);
+    const [data, setData] = useState(undefined);
 
-  const getData = async () => {
-    let result = {};
-    await Promise.all(
-      ["identified", "unidentified"].map(async (entry) => {
-        const query = {
-          query: {
-            bool: {
-              must: [
-                {
-                  wildcard: {
-                    "path.keyword": {
-                      value: "/g/?*",
-                    },
-                  },
-                },
-              ],
-              ...(entry === "identified"
-                ? {
-                    filter: [
-                      {
-                        script: {
-                          script: "doc['topics'].length != 0",
+    const getData = async () => {
+        let result = {};
+        await Promise.all(
+            ["total", "identified"].map(async (entry) => {
+                const query = {
+                    query: {
+                        bool: {
+                            must: [
+                                {
+                                    wildcard: {
+                                        "path.keyword": {
+                                            value: "/g/?*",
+                                        },
+                                    },
+                                },
+                            ],
+                            ...(entry === "identified"
+                                ? {
+                                    filter: [
+                                        {
+                                            "script": {
+                                                "script": "doc['topics'].length != 0",
+                                            },
+                                        },
+                                    ],
+                                }
+                                : {}),
                         },
-                      },
-                    ],
-                  }
-                : {}),
-            },
-          },
-        };
-        const count = await getCount(query);
-        result[entry] = count;
-      })
-    );
-    setData(result);
-  };
+                    },
+                };
+                const count = await getCount(query);
+                result[entry] = count;
+            })
+        );
+        setData(result);
+    };
 
-  useEffect(() => {
-    getData();
-  }, []);
+    useEffect(() => {
+        getData();
+    }, []);
 
-  return <div>{data ? <div>data</div> : <Loading />}</div>;
+    return (
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+        }}>
+            {data ? (
+                <Card sx={{ width: '50%' }}>
+                    <CardContent>
+                        <Typography variant="h6">
+                            {data.identified} / {data.total} ({100 * (data.identified / data.total).toFixed(3)} %)
+                        </Typography>
+                        <Typography color="gray">
+                            topics identified from requests to path /g/[ID]
+                        </Typography>
+                    </CardContent>
+                </Card>
+            ) : <Loading />}
+        </div>
+    )
+    //return <div>{data ? <div>{data.identified}</div> : <Loading />}</div>;
 }
 
 export default TopicIdentification;
