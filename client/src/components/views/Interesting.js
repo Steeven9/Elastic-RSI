f0c334344ea00fb386bc8635951d3a29557f8737
bohnhofl <bohnhofl@student.ethz.ch>
2022-05-29 19:54:59 +0200

    add spacing between cards

--- client/src/components/views/Interesting.js
+++ client/src/components/views/Interesting.js
@@ -3,15 +3,15 @@ import TopicIdentification from "../charts/TopicIdentification";
 import UnknownCountries from "../charts/UnknownCountries";
 
 const Interesting = () => {
-    return (
-        <>
-            <div style={{ marginBottom: '10px' }}>
-                <TopicIdentification />
-            </div>
-            <div style={{ marginBottom: '10px' }}>
-                <UnknownCountries />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div style={{ marginBottom: "10px" }}>
+        <TopicIdentification />
+      </div>
+      <div style={{ marginBottom: "10px" }}>
+        <UnknownCountries />
+      </div>
+    </>
+  );
 };
 export default Interesting;
