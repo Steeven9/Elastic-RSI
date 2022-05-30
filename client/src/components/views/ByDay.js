8a69484dcb43b5b4db71af35c2d6b50de88bfc90
bohnhofl <bohnhofl@student.ethz.ch>
2022-05-30 10:54:04 +0200

    add spacing in miscellaneous page

--- client/src/components/views/ByDay.js
+++ client/src/components/views/ByDay.js
@@ -5,8 +5,12 @@ import DeviceDayComparison from "../charts/DeviceDayComparison";
 const ByDay = () => {
   return (
     <>
-      <DayTimesComparison />
-      <DeviceDayComparison />
+      <div style={{ marginBottom: "30px" }}>
+        <DayTimesComparison />
+      </div>
+      <div style={{ marginBottom: "0px" }}>
+        <DeviceDayComparison />
+      </div>
     </>
   );
 };
